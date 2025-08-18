1760e282acef986831064534b9fd9c35b4ef6e10
abenezer@csa <abenezerhaymanot@gmail.com>
2025-08-18 16:34:28 +0300

    get_by_catagory implemeted with insance performacne boots

--- src/backend/server_backend.rs
+++ src/backend/server_backend.rs
@@ -14,6 +14,34 @@ pub async fn insert_category(category: String) -> Result<i64, ServerFnError> {
         .map_err(|e| ServerFnError::new(format!("Failed to insert category: {e}")))?;
     Ok(result.last_insert_rowid())
 }
+#[server]
+pub async fn get_by_category(
+    category: String,
+    offset: i64,
+    limit: i64,
+) -> Result<Vec<ImageDB>, ServerFnError> {
+    let db = get_db().await;
+    let rows: Vec<ImageDBSQL> =
+        sqlx::query_as("SELECT * FROM images WHERE category = ?1 LIMIT ?2 OFFSET ?3")
+            .bind(&category)
+            .bind(limit)
+            .bind(offset)
+            .fetch_all(db)
+            .await
+            .map_err(|e| ServerFnError::new(format!("DB error: {e}")))?;
+
+    let img_stack = rows
+        .into_iter()
+        .map(|row| ImageDB {
+            id: Some(row.id),
+            content: row.content,
+            name: row.name,
+            category: row.category,
+        })
+        .collect();
+
+    Ok(img_stack)
+}
 
 #[server]
 pub async fn insert_image(
@@ -50,25 +78,4 @@ pub async fn get_all_images() -> Result<Vec<ImageDB>, ServerFnError> {
         img_stack.push(all_images);
     }
     Ok(img_stack)
-}
-#[server]
-pub async fn get_single_todo(category: String) -> Result<ImageDB, ServerFnError> {
-    let db = get_db().await;
-    let rows: Vec<ImageDBSQL> = sqlx::query_as("SELECT * FROM images WHERE category LIKE ?1")
-        .bind(&category)
-        .fetch_all(db)
-        .await
-        .unwrap();
-    if rows.len() == 0 {
-        let msg = format!("Todo category : {} Not Found.", category);
-        Err(ServerFnError::new(msg))
-    } else {
-        let all_images = ImageDB {
-            id: Some(rows[0].id.clone()),
-            content: rows[0].content.clone(),
-            name: rows[0].name.clone(),
-            category: rows[0].category.clone(),
-        };
-        Ok(all_images)
-    }
 }
